9decbe0f081a3a13e3309aec80aaed3e23f801d2
Abhishekhemanth <hemanthreddy16699@gmail.com>
2022-05-22 18:50:48 +0530

    Whatsapp-Clone-mern

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGjVWmTwTGhOLQcxk9U6PrqxKpxuxrgp8",
+  authDomain: "whatsapp-clone-mern-df35d.firebaseapp.com",
+  projectId: "whatsapp-clone-mern-df35d",
+  storageBucket: "whatsapp-clone-mern-df35d.appspot.com",
+  messagingSenderId: "133937079810",
+  appId: "1:133937079810:web:b62e3bbaa00d8628ff291b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Sidebar.css"
+//rfce
+function sidebar() {
+  return (
+    <div className='sidebar'>sidebar</div>
+  )
+}
+
+export default sidebar
